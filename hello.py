449509a2cb085a8d3ebabfd676215ac68d0663b0
vinhara-ec22063 <vinhara-ec22063@stu.kln.ac.lk>
2025-01-17 19:40:47 +0530

    get inputs and add them

--- hello.py
+++ hello.py
@@ -1,4 +1,3 @@
-print ("Hello")
-print ("Good morning")
-name=input("Enter your name: ")
-print("Welcome ",name)
+num1=int(input("Enter the first number: "))
+num2=int(input("Enter the second number: "))
+print("Sum 0f ",num1," and ",num2," : ",num1+num2)
